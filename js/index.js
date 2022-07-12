32d4ff12f957dc78154e49e2ae780ddc8ab430db
Liana Yakovchuk <yakovchuk00y@gmail.com>
2022-07-12 11:55:23 +0100

    feat: API request

--- js/index.js
+++ js/index.js
@@ -14,42 +14,50 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+ 
+(async () => {
+  var breeds = await (await fetch('https://api.thecatapi.com/v1/breeds')).json();
+  var breedSelect = document.getElementById('breedSelect');
+  breedSelect.options.length = 0;
+  breeds.map(breed => {
+    breedSelect.options[breedSelect.options.length] = new Option(breed.name, breed.id);
+  }); 
+})();
+
+async function getBreeds() {
+  
 
 
+  var container = document.querySelector('.nav-display-result');
 
-const firstElm = document.querySelector('.tablinks');
-const otherElm = document.querySelector('.menu-btn');
-firstElm.addEventListener('mouseover', (e) => {
-  otherElm.style.outline = "3px solid red"
-  firstElm.style.outline = "3px solid red"
-})
-otherElm.addEventListener('mouseover', (e) => {
-  firstElm.style.outline = "3px solid red"
-  otherElm.style.outline = "3px solid red"
-})
-firstElm.addEventListener('mouseout', (e) => {
-  firstElm.style.outline = ""
-  otherElm.style.outline = ""
-})
-otherElm.addEventListener('mouseout', (e) => {
-  firstElm.style.outline = ""
-  otherElm.style.outline = ""
-})
+  var type = document.getElementById('typeSelect')
+  var typeValue = type.value
 
-firstElm.addEventListener('click', (e) => {
-  otherElm.style.outline = "3px solid blue"
-  firstElm.style.outline = "3px solid blue"
-})
-otherElm.addEventListener('click', (e) => {
-  firstElm.style.outline = "3px solid blue"
-  otherElm.style.outline = "3px solid blue"
-})
-firstElm.addEventListener('mouseout', (e) => {
-  firstElm.style.outline = ""
-  otherElm.style.outline = ""
-})
-otherElm.addEventListener('mouseout', (e) => {
-  firstElm.style.outline = ""
-  otherElm.style.outline = ""
-})
+  var order = document.getElementById('orderSelect')
+  var orderValue = order.value
 
+  var limit = document.getElementById('limitSelect')
+  var limitValue = limit.value
+
+  var breed = document.getElementById('breedSelect')
+  var breedValue = breed.value
+
+  var container = document.querySelector('.nav-display-result');
+
+  var params = new URLSearchParams();
+  params.set('limit', limitValue);
+  params.set('mime_types', typeValue);
+  params.set('order', orderValue);
+  params.set('breed_id', breedValue);
+  
+  var searchParams = await(await fetch('https://api.thecatapi.com/v1/images/search?' + params, {
+  headers: {
+    "x-api-key": "27958382-a5cb-4854-86b7-fc4cf6850b66"
+  }})).json();
+//display the result in container
+searchParams.map(result => {
+  var newImg = new Image();
+  newImg.src = result.url;
+  container.appendChild(newImg);
+})    
+}
